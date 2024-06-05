26411e3a51785799070db5c1f19c5d7e80e3d386
uliana <ulianaevstigneeva@yandex.ru>
2024-06-05 23:17:53 +0300

    Initial Commit

---
+++ Walkthrough/WalkthroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkthroughApp.swift
+//  Walkthrough
+//
+//  Created by Ульяна Евстигнеева on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WalkthroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
