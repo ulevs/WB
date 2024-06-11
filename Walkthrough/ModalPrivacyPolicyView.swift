990445d253bf99e113c7b6f5802999b44ad34da7
uliana <ulianaevstigneeva@yandex.ru>
2024-06-11 23:42:49 +0300

    .sheet and dismiss

---
+++ Walkthrough/ModalPrivacyPolicyView.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyPolicyView.swift
+//  Walkthrough
+//
+//  Created by Ульяна Евстигнеева on 11.06.2024.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrivacyPolicyView()
+}
---
+++ Walkthrough/ModalTermsOfUseView.swift
@@ -0,0 +1,18 @@
+//
+//  ermsOfUseView.swift
+//  Walkthrough
+//
+//  Created by Ульяна Евстигнеева on 11.06.2024.
+//
+
+import SwiftUI
+
+struct TermsOfUseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsOfUseView()
+}
