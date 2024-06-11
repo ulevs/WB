7ba3925b4ee31bbe191a1ad6f82625d34cfa4d27
uliana <ulianaevstigneeva@yandex.ru>
2024-06-11 15:26:46 +0300

    onboarding

--- Walkthrough/ContentView.swift
+++ Walkthrough/ContentView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("walktroughImage")
+                .resizable()
+                .frame(width: 262, height: 271)
+                .padding(.top, 135)
+            
+            HeadlineTextView(title: "Общайтесь с друзьями и близкими легко")
+                .padding(.top, 42)
+            
+            Spacer()
+            
+            Text(.init("""
+            Нажимая кнопку продолжить я соглашаюсь с
+            \("[Политикой Конфиденциальности](https://google.com)") и \("[Условиями Использования](https://google.com)")
+           """))
+                .font(.system(size: 10))
+                .foregroundStyle(.gray)
+                .tint(.wbDefault)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 13)
+                .lineSpacing(4)
+            
+            ButtonWBView(title: "Начать общаться", action: {})
         }
-        .padding()
+        .background(.wbBG)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
--- Walkthrough/ViewComponents/ButtonWBView.swift
+++ Walkthrough/ViewComponents/ButtonWBView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ButtonWBView: View {
+    let title: String
+    let action: ()->()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action:action) {
+            Text(title)
+                .font(.system(size: 16))
+                .foregroundStyle(.white)
+                .padding()
+        .frame(maxWidth: .infinity)
+        .background(.wbButton)
+        .cornerRadius(30)
+        }
+        .padding(.horizontal, 24)
+        .padding(.bottom, 40)
     }
 }
 
 #Preview {
-    ButtonWBView()
+    ButtonWBView(title: "button", action: {})
 }
+//struct ButtonWB: View {
+//    let title: String
+//    let action: ()->()
+//    var body: some View {
+//        Button(action:action) {
+//            Text(title)
+//                .font(.system(size: 16))
+//                .foregroundStyle(.white)
+//                .padding()
+//        .frame(maxWidth: .infinity)
+//        .background(.wbButton)
+//        .cornerRadius(30)
+//        }
+//        .padding(.bottom, 40)
+//    }
+//}
